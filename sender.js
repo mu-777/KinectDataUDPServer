3d743290e4679fe6465a076c21556bd89ea11396
RyosukeMurata <ryosuke.murata.7@gmail.com>
2015-06-02 08:29:51 +0900

    able to change ip address easily

--- sender.js
+++ sender.js
@@ -7,12 +7,14 @@
     var Kinect2 = require('kinect2'),
         kinect = new Kinect2(),
         sender = require('dgram').createSocket("udp4"),
+        ipAddress = "10.249.255.191",//"localhost",
+        port = 8000,
         message = {};
 
     if (kinect.open()) {
         kinect.on('bodyFrame', function (bodies) {
             message = new Buffer(JSON.stringify(bodies[0].joints3d));
-            sender.send(message, 0, message.length, 8000, "localhost");
+            sender.send(message, 0, message.length, port, ipAddress);
             for (var i = 0; i < bodies.length; i++) {
                 console.log(bodies[i].joints3d);
             }
